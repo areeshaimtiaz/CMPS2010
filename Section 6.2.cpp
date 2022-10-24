fd405a1488757b4f961cb5b06a42a3c13f67c39a
areeshaimtiaz <76179142+areeshaimtiaz@users.noreply.github.com>
2022-10-24 14:34:03 -0700

    Add files via upload

---
+++ Section 6.2.cpp
@@ -0,0 +1,26 @@
+// Textbook Question 34
+#include <iostream>
+using namespace std;
+
+int cube(int num);
+
+int main()
+{
+    int result;
+    result = cube(4);
+    cout << "The cube of 4 is " << result << endl;
+    return 0;
+}
+
+int cube(int num)
+{
+    return num * num * num;
+}
+
+
+
+
+
+
+
+
