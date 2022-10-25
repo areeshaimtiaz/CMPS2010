c1f176f9c65b525fb458098f99cbb42ed8dd1bc1
areeshaimtiaz <76179142+areeshaimtiaz@users.noreply.github.com>
2022-10-24 19:58:03 -0700

    Add files via upload

---
+++ Section 6.13.cpp
@@ -0,0 +1,37 @@
+// Textbook Programming Challenges: 8. Coin Toss
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void coinToss();
+
+int main()
+{
+    int number;
+    string result;
+    cout << "How many times should the coin be tossed: ";
+    cin >> number;
+
+    for(int i = 1; i <= number; i++)
+    {
+        cout << i << ". ";
+        coinToss();
+    }
+    return 0;
+}
+
+void coinToss()
+{
+    int number;
+    number = rand() % 2;
+
+    if (number == 0)
+    {
+        cout << "Heads" << endl;
+    }
+    else
+    {
+        cout << "Tails" << endl;
+    }
+}
