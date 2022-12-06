131d1960a9bf8ae0c51e5e7e9fd45161ef11ea64
areeshaimtiaz <76179142+areeshaimtiaz@users.noreply.github.com>
2022-12-06 00:27:08 -0800

    Add files via upload

---
+++ Course.cpp
@@ -0,0 +1,13 @@
+// Section 14.7 - Aggregation
+
+#include <iostream>
+#include <string>
+#include "Course.h"
+using namespace std;
+
+int main()
+{
+    Course myCourse("Mathematics", "Todorova", "Yana", "1050", "Precalculus I", "David Lippman", "World Publisher"); // Initialise
+    myCourse.print();
+    return 0;
+}
